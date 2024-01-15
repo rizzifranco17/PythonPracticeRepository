2b4c25a5342cfc7d6c35a8cc04bf1d88c5c0719c
rizzifranco17 <rizzifranco17@gmail.com>
2024-01-15 12:13:59 +0100

    commit

---
+++ 13_modules.py
@@ -0,0 +1,18 @@
+
+import module
+#from 10_functions import sum_two_values No se pueden empezar ficheros por numero en Py
+from module import sum, printValue
+
+module.sum (4,2,3)
+module.printValue ("hello python")
+sum (7,4,5)
+printValue ("Hello python")
+
+
+from math import pi
+
+#print(math.pi)
+#print(math.cos(6))
+#print(math.pow(2,8))
+
+print (pi + sum)
---
+++ module.py
@@ -0,0 +1,6 @@
+def sum(number_one, number_two, nummber_three) :
+    print (number_one + number_two + nummber_three) 
+
+
+def printValue (value):
+    print(value)
