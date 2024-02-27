143b12ef7232e1a2ca8fd1d3237e7226ea59c781
rizzifranco17 <rizzifranco17@gmail.com>
2024-02-27 14:59:39 +0100

    Commit

---
+++ 16.3_Challenges.py
@@ -0,0 +1,20 @@
+'''Escribe un programa que se encargue de comprobar si un numero
+es o no primo. 
+Hecho estom impre los numeros primos entre 1 y 100'''
+
+
+
+def is_prime(number):
+    if number < 2:
+        return False 
+    elif number / number == 0:
+        return True 
+    elif number / 1 == 0: 
+   
+
+
+     return True
+
+
+
+is_prime(2)
