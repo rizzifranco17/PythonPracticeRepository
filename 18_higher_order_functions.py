b282e3cc082f8d3d7d68d65d3af7fe8f413983cd
rizzifranco17 <rizzifranco17@gmail.com>
2024-02-29 12:36:51 +0100

    Commit

--- 18_higher_order_functions.py
+++ 18_higher_order_functions.py
@@ -1,5 +1,8 @@
 
 
+from functools import reduce
+
+
 def sum_one (value):
     return value + 1
 
@@ -11,3 +14,46 @@ def sum_two_values_and_one (first_value, second_value,f_sum):
 
 print (sum_two_values_and_one(3,2,sum_one))
 print (sum_two_values_and_one(3,2,sum_five))
+
+# Closures # 
+
+def sum_ten (original_value): 
+    def add (value):
+        return value + 10 + original_value
+    return add
+ #Function returnig function 
+add_closure = sum_ten (10)
+print (add_closure(15))
+
+# Build-in Higher Order Functions 
+
+number = [2,5,10,21,3,45]
+
+# Map
+
+def multuply_by_two (number):
+    return number * 2
+
+
+#print(list( map (multuply_by_two,number)))
+
+print(list( map (lambda number :number *2,number)))
+# I can use a lambda a get the same result without defining another function 
+#Filter 
+def filter_greater_that_ten(number):
+    if number > 10:
+        return True
+    return False
+print(list (filter (filter_greater_that_ten, number)))
+print(list (filter (lambda number: number >10, number)))
+
+#Reduce
+
+[2,5,10,21,3,45]
+
+def sum_two_values (first_Value, second_value):
+    print (first_Value)
+    print (second_value)
+    return first_Value + second_value
+
+print(reduce (sum_two_values,number))
