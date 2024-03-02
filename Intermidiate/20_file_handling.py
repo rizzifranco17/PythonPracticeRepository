47fd807a6e6003e4229bd24d436f5165a892b2a8
rizzifranco17 <rizzifranco17@gmail.com>
2024-03-02 21:38:21 +0100

    commit

---
+++ Intermidiate/20_file_handling.py
@@ -0,0 +1,49 @@
+import os
+
+#txt_file = open("/Users/francorizzi/Code/PythonScripting/PythonPracticeRepository/Intermidiate/my_file.txt", "r+")
+#txt_file.write("Mi nombre es Franco\nMi apellido es Rizzi\nTengo 27 años\nMi lenguaje es Python\nAGUANTE BOCA\n Y el Celeste del imperio") 
+
+
+#print (txt_file.read(10)) 
+#print(txt_file.readline())
+#print(txt_file.readlines())
+#for line in txt_file.readlines ():
+    #print (line)
+
+#txt_file.write("\n Y el celeste del imperio ")
+#txt_file.close()
+#os.remove ("/Users/francorizzi/Code/PythonScripting/PythonPracticeRepository/Intermidiate/my_file.txt")
+
+
+#.json file 
+
+#import json
+
+#json_file = open ("/Users/francorizzi/Code/PythonScripting/PythonPracticeRepository/Intermidiate/my_file.json", "w+")
+
+#json_test = {
+   # "Name": "Franco",
+    #"Lastname" :"Rizzi",
+    #"Age": 27,
+    #"Languaje":["Spanish", "English", "Italian"]}
+
+#json.dump(json_test,json_file, indent= 2)
+
+#json_file.close()
+
+#with open ("/Users/francorizzi/Code/PythonScripting/PythonPracticeRepository/Intermidiate/my_file.json") as my_other_file:
+    #for line in my_other_file.readlines():
+          #print(line)
+
+#json_dict= json.load(open ("/Users/francorizzi/Code/PythonScripting/PythonPracticeRepository/Intermidiate/my_file.json"))
+#print(json_dict)
+#print(type(json_dict))
+#print (json_dict["Name"])
+
+
+#.cvs 
+import csv 
+
+csv_file = open ("/Users/francorizzi/Code/PythonScripting/PythonPracticeRepository/Intermidiate/my_file.csv")
+
+import xml
