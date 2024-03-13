29467526a9e6b951f94d2ab073f422fd05dd05a6
rizzifranco17 <rizzifranco17@gmail.com>
2024-03-13 19:06:58 +0100

    Commit

---
+++ Intermidiate/22_python_package_manager.py
@@ -0,0 +1,24 @@
+
+#PIP https://pypi.org
+
+import numpy
+print (numpy.version.version)
+
+numpy_array= numpy.array ([35,24,62,52,30,30,17])
+print (type(numpy_array))
+print (numpy_array *2)
+
+import pandas 
+
+#pip list
+
+import requests
+
+response = requests.get ("https://pokeapi.co/api/v2/pokemon?limit=151")
+print (response)
+print (response.status_code)
+print (response.json())
+
+# Arithmetics Package
+from mypackage import arithmetics
+print (arithmetics.sum_two_values(1,4))
