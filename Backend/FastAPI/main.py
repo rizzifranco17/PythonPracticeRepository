afed6ad27c3ff531c45aae3b132a40ab1168faf2
rizzifranco17 <rizzifranco17@gmail.com>
2024-05-03 14:59:12 +0200

    Commit

--- Backend/FastAPI/main.py
+++ Backend/FastAPI/main.py
@@ -4,9 +4,5 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return "Hola FastAPI!"
-
-@app.get ("/url")
-async def url():
-    return{ "url":"https://mouredev.com/python"}
+    return "Bocaaaaaaa"
 
